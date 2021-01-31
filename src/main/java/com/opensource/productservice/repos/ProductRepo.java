b9378dad11c10b9f0754d3d084df57f998ccaa79
aakash <aakash>
2021-01-31 19:15:26 +0530

    Model and Repo created

---
+++ src/main/java/com/opensource/productservice/repos/ProductRepo.java
@@ -0,0 +1,9 @@
+package com.opensource.productservice.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.opensource.productservice.model.Product;
+
+public interface ProductRepo extends JpaRepository<Product, Long> {
+
+}
